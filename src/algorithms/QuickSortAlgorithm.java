1d05bc5bb146cbd83e403edba2246c3880674c40
Louis Matthijssen <louis@u5r.nl>
2017-02-19 00:03:43 +0100

    QuickSort

--- src/algorithms/QuickSortAlgorithm.java
+++ src/algorithms/QuickSortAlgorithm.java
@@ -2,6 +2,9 @@ package algorithms;
 
 import models.DataSetModel;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class QuickSortAlgorithm extends Algorithm {
     public QuickSortAlgorithm(DataSetModel dataSet) {
         super(dataSet);
@@ -9,6 +12,28 @@ public class QuickSortAlgorithm extends Algorithm {
 
     @Override
     public boolean nextStep() {
-        return false;
+        quickSort(data, 0, data.size() -1);
+        return true;
+    }
+
+    private void quickSort(LinkedList<Integer> numbers, int low, int high) {
+        if (low < high) {
+            int p = partition(numbers, low, high);
+            quickSort(numbers, low, p - 1);
+            quickSort(numbers, p + 1, high);
+        }
+    }
+
+    private int partition(LinkedList<Integer> numbers, int low, int high) {
+        int pivot = numbers.get(high);
+        int i = low - 1;
+        for (int j = low; j < high; j++)  {
+            if (numbers.get(j) <= pivot) {
+                i++;
+                Collections.swap(numbers, i, j);
+            }
+        }
+        Collections.swap(numbers, i+ 1, high);
+        return i + 1;
     }
 }
