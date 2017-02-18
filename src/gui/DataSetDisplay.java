ac6a703a7e736d5c59ca9dbbf6c926020af8d4e7
Louis Matthijssen <louis@u5r.nl>
2017-02-18 18:01:10 +0100

    Draw number in bar

--- src/gui/DataSetDisplay.java
+++ src/gui/DataSetDisplay.java
@@ -23,16 +23,38 @@ public class DataSetDisplay extends JPanel {
     }
 
     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         DataSetModel dataSet = dataBundle.getDataSetModel();
 
         int heightPerBar = getHeight() / dataSet.getHighestNumber();
         int widthPerBar = getWidth() / dataSet.getHighestNumber();
 
         int positionX = 0;
-        for (int number: dataSet) {
-            int barHeight = heightPerBar * number;
-            g.fillRect(positionX, getHeight() - barHeight, widthPerBar, barHeight);
-            positionX += widthPerBar;
-        }
+        for (Integer number: dataSet)
+            positionX = drawBar(g, number, positionX, heightPerBar, widthPerBar);
+    }
+
+    private int drawBar(Graphics graphics, Integer number, int initialX, int barHeight, int barWidth) {
+        barHeight = barHeight * number;
+        int positionY = getHeight() - barHeight;
+        Rectangle barRectangle = new Rectangle(initialX, positionY, barWidth, barHeight);
+
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(barRectangle.x, barRectangle.y, barRectangle.width, barRectangle.height);
+
+        drawNumberInBar(graphics, number, barRectangle);
+
+        return initialX + barWidth;
+    }
+
+    private void drawNumberInBar(Graphics graphics, Integer number, Rectangle barRectangle) {
+        String numberText = number.toString();
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+
+        int x = ((barRectangle.width - fontMetrics.stringWidth(numberText)) / 2) + barRectangle.x;
+        int y = barRectangle.y + fontMetrics.getHeight();
+
+        graphics.setColor(Color.WHITE);
+        graphics.drawString(numberText, x, y);
     }
 }
