4b963e718858791ad2a9ca320d0554840f3c5592
Louis Matthijssen <louis@u5r.nl>
2017-03-02 19:19:02 +0100

    Adjust bar colors

--- src/data/Settings.java
+++ src/data/Settings.java
@@ -14,7 +14,7 @@ public class Settings {
     public static final int AUTO_NEXT_STEP_SIZE = 10;
 
     public static final Color BACKGROUND_COLOR = Color.web("#CFD8DC");
-    public static final Color BAR_COLOR = Color.web("#2962FF");
-    public static final Color BAR_SORTED_COLOR = Color.web("#1B5E20");
-    public static final Color BAR_COMPARED_COLOR = Color.web("#D50000");
+    public static final Color BAR_COLOR = Color.web("#1E88E5");
+    public static final Color BAR_SORTED_COLOR = Color.web("#43A047");
+    public static final Color BAR_COMPARED_COLOR = Color.web("#0D47A1");
 }
